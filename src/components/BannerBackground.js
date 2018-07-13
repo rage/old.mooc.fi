719e334714019fbd020a9a32c2bf9967ceffead7
Henrik Nygren <nygrenh@gmail.com>
2018-07-13 11:11:24 +0300

    Add backdrop image

--- src/components/BannerBackground.js
+++ src/components/BannerBackground.js
@@ -1,16 +1,18 @@
 import React from "react";
 import styled from "styled-components";
 
+import Background from "../images/laptops.jpg";
+
 const Headline = styled.div`
   position: absolute;
   top: 0;
   left: 0;
   align-content: center;
   justify-content: center;
-  background-color: #1a7f89;
+  background: url(${Background});
   background-repeat: no-repeat;
-  background-position: bottom right;
-  background-size: 60%;
+  background-position: top center;
+  background-size: cover;
   height: 700px;
   width: 100%;
   z-index: -100;
