fac045fca8a58f92c3d806684b29b59c0b5d15c0
Antti Leinonen <antti.leinonen@helsinki.fi>
2018-07-13 16:56:15 +0300

    Incorrect english urls show english 404 page

--- src/pages/Redirector.js
+++ src/pages/Redirector.js
@@ -1,16 +1,18 @@
-import React from 'react';
-import { Redirect } from 'react-router-dom'
-import { redirects } from '../constants/Redirects'
+import React from "react";
+import { Redirect } from "react-router-dom";
+import { redirects } from "../constants/Redirects";
 
-export default ({location}) => {
-  const foundPath = redirects.find(redirect => redirect.from === location.pathname)
-  if (!foundPath) {
-    return(
-      <Redirect to={"/404"} />
-    )
+export default ({ location }) => {
+  const foundPath = redirects.find(
+    redirect => redirect.from === location.pathname
+  );
+  if (!foundPath && location.pathname.startsWith("/en/")) {
+    return <Redirect to={"/en/404"} />;
+  } else if (!foundPath) {
+    return <Redirect to={"/404"} />;
   }
-  const whereTo = redirects.find(redirect => redirect.from === location.pathname).to
-  return(
-    <Redirect to={whereTo} />
-  )
+  const whereTo = redirects.find(
+    redirect => redirect.from === location.pathname
+  ).to;
+  return <Redirect to={whereTo} />;
 };
