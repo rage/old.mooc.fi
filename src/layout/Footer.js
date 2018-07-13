9044e9643bbf8ddde1927210912af6bedcf8d4b9
Henrik Nygren <nygrenh@gmail.com>
2018-07-13 11:12:08 +0300

    Add content to footer

--- src/layout/Footer.js
+++ src/layout/Footer.js
@@ -1,9 +1,20 @@
 import React from "react";
 import styled from "styled-components";
 
+import UHLogo from "../images/uh-logo.png";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTwitter, faFacebook } from "@fortawesome/free-brands-svg-icons";
+
+const StyledIcon = styled(FontAwesomeIcon)`
+  color: white;
+  margin-bottom: 1rem;
+  margin: 1rem;
+`;
+
 const Footer = styled.footer`
   height: 20rem;
   display: flex;
+  flex-direction: column;
   align-items: center;
   justify-content: center;
   background-color: #202124 !important;
@@ -12,4 +23,37 @@ const Footer = styled.footer`
   text-align: center;
 `;
 
-export default () => <Footer>Please consider the environment before printing this footer.</Footer>;
+const SocialContainer = styled.div`
+  padding: 2rem 0;
+`;
+
+const EmailContainer = styled.div`
+  padding-top: 2rem;
+`
+
+export default () => (
+  <Footer>
+    <EmailContainer>mooc@cs.helsinki.fi</EmailContainer>
+    <SocialContainer>
+      <a
+        href="https://twitter.com/moocfi"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <StyledIcon icon={faTwitter} size="3x" />
+      </a>
+      <a
+        href="https://www.facebook.com/Moocfi"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <StyledIcon icon={faFacebook} size="3x" />
+      </a>
+    </SocialContainer>
+    <div>
+      <a href="https://helsinki.fi">
+        <img alt="University of Helsinki" src={UHLogo} />
+      </a>
+    </div>
+  </Footer>
+);
