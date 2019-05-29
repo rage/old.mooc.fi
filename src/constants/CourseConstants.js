5fe388a9e1320eb723def7db2e8868080903295f
Matti Luukkainen <mluukkai@iki.fi>
2019-05-29 17:22:49 +0300

    Update CourseConstants.js

--- src/constants/CourseConstants.js
+++ src/constants/CourseConstants.js
@@ -75,7 +75,7 @@ export const finnishCourses = {
       description:
         "Syväsukellus moderniin websovelluskehitykseen — ota haltuusi React, Redux, Node.js, GraphQL ja MongoDB! Kurssi alkaa 15.3. ja päättyy 15.12.",
       image: FullStackImage,
-      href: "https://fullstackopen-2019.github.io/",
+      href: "href: "https://fullstackopen.com/",
     },
     {
       name: "Elements of AI",
