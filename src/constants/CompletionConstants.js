e75e9ddbec739d6a85f8fe78ab1fb6596bb2ae01
Henrik Nygren <nygrenh@gmail.com>
2019-06-19 16:43:39 +0300

    Update ai urls

--- src/constants/CompletionConstants.js
+++ src/constants/CompletionConstants.js
@@ -4,13 +4,13 @@ const CompletionConstants = {
       name: "Elements of AI",
       materialUrl: "https://course.elementsofai.com/",
       formUrl:
-        "https://www.avoin.helsinki.fi/palvelut/esittely.aspx?o=126531969"
+        "https://www.avoin.helsinki.fi/palvelut/esittely.aspx?o=129202817"
     },
     finnish: {
       name: "Elements of AI",
       materialUrl: "https://course.elementsofai.com/fi",
       formUrl:
-        "https://www.avoin.helsinki.fi/palvelut/esittely.aspx?o=127290002"
+        "https://www.avoin.helsinki.fi/palvelut/esittely.aspx?o=129202330"
     }
   }
 };
